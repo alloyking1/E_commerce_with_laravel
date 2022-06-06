00006dbb24f941973b9bfbedc81ad7ae94f84be8
alloyking1 <alloyking1@gmail.com>
2022-06-06 11:26:28 +0100

    feat:implemented adding to cart

--- app/Http/Livewire/ProductSelector.php
+++ app/Http/Livewire/ProductSelector.php
@@ -2,14 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Variation;
 use Livewire\Component;
+
 
 class ProductSelector extends Component
 {
 
     public $product;
     public $initialVariation;
+    public $skuVariant;
 
+    protected $listeners = [
+        'skuVariantSelected'
+    ];
+
+    public function skuVariantSelected($variantId)
+    {
+        if (!$variantId) {
+            $this->skuVariant = null;
+            return;
+        }
+
+        $this->skuVariant = Variation::find($variantId);
+        // dd($this->skuVariant);
+    }
+
+    public function addToCart()
+    {
+        dd($this->skuVariant);
+    }
 
     public function mount()
     {
