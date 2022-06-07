3fe621b857a9d167665e66e99b4c47bcda8fc1db
alloyking1 <alloyking1@gmail.com>
2022-06-07 09:18:49 +0100

    product image thumbnail added

--- resources/views/livewire/product-galary.blade.php
+++ resources/views/livewire/product-galary.blade.php
@@ -2,8 +2,12 @@
     <img src=" {{$selectedImageUrl}}">
 
     <div class="grid grid-cols-6 gap-2">
+        @foreach ($product->getMedia() as $media)
         <button>
-            <img src="">
+            <img src="{{$media->getUrl('thumb200x200')}}">
         </button>
+
+        @endforeach
+
     </div>
 </div>
